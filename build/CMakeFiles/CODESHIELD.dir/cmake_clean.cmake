f84dc2a9b90740ffd786a8f5b143647196e76180
eduardodelfin <eduardodelfin10@gmail.com>
2026-05-04 23:03:53 -0400

    estructura inicial y lista doble de enemigos

---
+++ build/CMakeFiles/CODESHIELD.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CODESHIELD.dir/src/main.cpp.o"
+  "CMakeFiles/CODESHIELD.dir/src/main.cpp.o.d"
+  "CODESHIELD"
+  "CODESHIELD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CODESHIELD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
